8adb93cb6a4d32ef1b4ed2afc2754ec89e5d647c
wSTONE80085 <wyatt.stone@stu.daviess.kyschools.us>
2025-02-12 04:34:45 +0000

    done

--- ArrayAverages.java
+++ ArrayAverages.java
@@ -9,15 +9,52 @@ public class ArrayAverages{
   }
 
   public void rowAverages(){
-    
+    int avg = 0;
+    int sum = 0;
+    for(int row = 0; row<ray.length; row++)
+    {
+      avg = 0;
+      sum = 0;
+      for(int col = 0; col<ray[row].length; col++)
+        {
+          sum += ray[row][col];
+          avg++;
+        }
+          avg = sum/avg;
+    }
+    System.out.println("The average of the row " + row + " is: " + avg);
   }
 
   public void columnAverages(){
-
+    int avg = 0;
+    int sum = 0;
+    for(int col = 0; col<ray[0].length; col++)
+    {
+      avg = 0;
+      sum = 0;
+      for(int row = 0; row<ray.length; row++)
+        {
+          sum += ray[row][col];
+          avg++;
+        }
+          avg = sum/avg;
+    }
+    System.out.println("The average of the column " +  col + " is: " + avg);
   }
 
   public void arrayAverage(){
-
+    int avg = 0;
+    int sum = 0;
+    for(int col =0; col<ray[0].length; col++)
+      {
+        for(int row=0; row<ray.length; row++)
+          {
+            sum+= ray[row][col];
+            avg++;
+          }
+      }
+            avg=sum/avg;
+        System.out.println("the average of the entire array is: " + avg);
   }
 
   public String toString(){
@@ -25,5 +62,7 @@ public class ArrayAverages{
     {
       System.out.println(Arrays.toString(ray[row]));
     }
+
+    return "";
   }
 }
--- Gee17.java
+++ Gee17.java
@@ -1,7 +1,12 @@
 public class Gee17{
   public static void main(String[] args){
-    TwoDee twoDee = new TwoDee();
 
+    int[][] ray = new int[7][7];
+
+
+     ArrayAverages twoDee = new ArrayAverages(ray);
+
+     System.out.println(twoDee);
     twoDee.rowAverages();
     twoDee.columnAverages();
     twoDee.arrayAverage();
